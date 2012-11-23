c22747f7e37c8ff268c130dce5c284d48431f026
Daniel Robbins <drobbins@funtoo.org>
2012-11-24 00:53:07 +0300

    bumping version to 1.97

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.libvirt"
-VERSION = "1.96"
+VERSION = "1.97"
 AUTHOR = "David Nicklay"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
